433defc7478d7e2eb177e16f208bdcd076dcd8f5
rohit negi <rohitnegi263139@gamil.com>
2025-04-09 20:42:17 +0530

    use moment to display message time

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import useSendMessage from "../hooks/useSendMessage.js";
 import useGetMessages from "../hooks/useGetMessages.js";
+import moment from "moment";
 
 const Chat = () => {
 	const user = useSelector((store) => store.user);
@@ -29,35 +30,48 @@ const Chat = () => {
 	};
 	return (
 		<div className="sm:w-3/4 bg-base-300 mx-auto border border-gray-600 m-5 h-[80vh] flex flex-col">
-			<h1 className="p-5 border-b border-gray-600">John Doe</h1>
-			<div className="flex-1 overflow-scroll p-5">
-				{chats &&
-					chats?.map((chat) => {
-						return (
-							<div
-								key={chat?._id}
-								className={`chat ${
-									chat?.senderId._id == user?._id ? "chat-end" : "chat-start"
-								}`}
-							>
-								<div className="chat-image avatar">
-									<div className="w-10 rounded-full">
-										<img alt="user image" src={chat?.senderId?.photoUrl} />
+			{chats && (
+				<>
+					<h1 className="p-5 border-b border-gray-600">
+						{+chats[0]?.receiverId
+							? "Chats with " + chats[0]?.receiverId?.firstName
+							: "Start Your chats now"}
+					</h1>
+					<div className="flex-1 overflow-scroll p-5 bg-slate-500">
+						{chats.length &&
+							chats?.map((chat) => {
+								return (
+									<div
+										key={chat?._id}
+										className={`chat ${
+											chat?.senderId?._id == user?._id
+												? "chat-end"
+												: "chat-start"
+										}`}
+									>
+										<div className="chat-image avatar">
+											<div className="w-10 rounded-full">
+												<img alt="user image" src={chat?.senderId?.photoUrl} />
+											</div>
+										</div>
+										<div className="chat-header">
+											{chat?.senderId?.firstName}
+											<time className="text-xs opacity-50">
+												{moment(chat?.createdAt).format("hh:mm")}
+											</time>
+										</div>
+										<div className="chat-bubble bg-base-200">
+											{chat?.message}
+										</div>
 									</div>
-								</div>
-								<div className="chat-header">
-									{chat?.senderId._id == user?._id
-										? chat?.senderId?.firstName
-										: chat?.receiverId?.firstName}
-									<time className="text-xs opacity-50">12:45</time>
-								</div>
-								<div className="chat-bubble bg-base-200">{chat?.message}</div>
-							</div>
-						);
-					})}
-			</div>
+								);
+							})}
+					</div>
+				</>
+			)}
 			<div className="p-5 border-t border-gray-600 flex items-center gap-2">
 				<input
+					placeholder="Type your message..."
 					value={message}
 					className="flex-1 border border-gray-500 rounded p-2"
 					onChange={(e) => setMessage(e.target.value)}
